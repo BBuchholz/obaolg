20f0bfa2c02fb2f5747ac857feb31e4474613557
Brent Buchholz <brentbuchholz@gmail.com>
2023-08-29 20:50:12 -0500

    Add PNDMN

---
+++ src/pages/myrKiSs/PNDMN.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>"PANDAEMONIUM STUDIO"</h1>
+  <p>
+    HISTORY GOES HERE
+  </p>
+  <p>---</p>
+  <p>WHEN LIVE, LINK WILL BE PLACED HERE</p>
+  <p>---</p>
+  <p>
+    this is a collection of early attempts that WE feel are
+    on the right track but are still growing, and so we post
+    them here as Ongoing Projeckts for Learning Participants,
+    we are always looking for collaborators so contact us
+    if your interest is piqued
+  </p>
+  <div class="card">
+    <a href="https://pandaemonium.netlify.app/" target="_blank">
+      <img src="/PNDMN-0813_CARD.png">
+    </a>
+  </div>
+  <h1>Establishments and Updates</h1>
+  <p mt-4 text-sm>
+    Sylvan Circle Academy
+    Exploratory Participant "Pandaemonic" <br>
+    Established This Exploratory Projeckt Link on 2023-08-29
+  </p>
+</template>
+
+<style>
+  a.highlighted {
+    color: red;
+  }
+  .card {
+    max-width: 50%;
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    border: 5px;
+    border-color: white;
+    border-style: solid;
+    border-radius: 1%;
+  }
+</style>
